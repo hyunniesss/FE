6dd775918b7e648f708157228224a55d91acff42
unknown <wjdgusgml997@naver.com>
2022-06-07 22:51:40 +0900

    ReactJS로 영화 웹 서비스 만들기 D1

---
+++ reactJS/class-component/src/App.js
@@ -0,0 +1,17 @@
+/*
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        const name = 'react';
+        return <div className="react">{name}</div>
+    }
+}
+
+*/
+import React from 'react';
+function App() {
+    const name = "리액트";
+    return <div className="react">{name}</div>
+}
+export default App;
